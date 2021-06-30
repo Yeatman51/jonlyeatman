cdd9f58798324b2c28630e3b68fbbf569e52d8f1
jon <jonlyeatman@gmail.com>
2021-06-30 16:52:59 -0400

    Primary code brought over from github pages repository vs 1.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,43 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css';
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route } from 'react-router-dom';
+import Header from './components/Header.js';
+import Navbar from './components/Navbar.js';
+import Wrapper from "./components/Wrapper.js";
+import Home from './pages/Home.js';
+import Developer from './pages/Developer.js';
+import Designer from './pages/Designer.js';
+import Photographer from './pages/Photographer.js';
+import About from './pages/About.js';
+import Contact from './pages/Contact.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+    <div className="App background-img">
+      
+
+      <Header/>
+
+      <HashRouter>
+      <Navbar/>
+      <Wrapper>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Developer" component={Developer} />
+          <Route exact path="/Designer" component={Designer} />
+          <Route exact path="/Photographer" component={Photographer} />
+          <Route exact path="/About" component={About} />  
+          <Route exact path="/Contact" component={Contact} />  
+      </Wrapper>
+      </HashRouter>
+
+      <Footer/>
+      
+
+      
     </div>
   );
 }
-
 
 export default App;
