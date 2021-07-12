72d0a87fc970fd251a65b927e634e89256b655ec
jon <jonlyeatman@gmail.com>
2021-07-11 22:06:20 -0400

    modal file prep

--- client/src/components/modals/modal_1.js
+++ client/src/components/modals/modal_1.js
@@ -3,7 +3,16 @@ import React from 'react'
 function modal_1() {
     return (
         <div>
-            
+            <div className="modal-container">
+                <h1 className="modal-title"></h1>
+                <img className="modal-img" src="" alt="" />
+                <p className="modal-description"></p>
+                <h2 className="modal-"></h2>
+                <p className="modal--"></p>
+
+            </div>
+
+            <div className="overlay" ref={modalRef} onClick={hideModal}></div>
         </div>
     )
 }
