8adf42438d5ce49736e9bc3d3608fb696753bb28
jon <jonlyeatman@gmail.com>
2021-09-20 16:06:57 -0400

    Optimize img 5–8

--- client/src/pages/Photographer.js
+++ client/src/pages/Photographer.js
@@ -27,22 +27,22 @@ function Photographer() {
     },
     {
       key: 5,
-      imgSrc:"/images/photos-img/Yeatman_5.jpg",
+      imgSrc:"/images/photos-lowq-img/Yeatman_5.jpg",
       alt:"pic"
     },
     {
       key: 6,
-      imgSrc:"/images/photos-img/Yeatman_6.jpg",
+      imgSrc:"/images/photos-lowq-img/Yeatman_6.jpg",
       alt:"pic"
     },
     {
       key: 7,
-      imgSrc:"/images/photos-img/Yeatman_7.jpg",
+      imgSrc:"/images/photos-lowq-img/Yeatman_7.jpg",
       alt:"pic"
     },
     {
       key: 8,
-      imgSrc:"/images/photos-img/Yeatman_8.jpg",
+      imgSrc:"/images/photos-lowq-img/Yeatman_8.jpg",
       alt:"pic"
     },
     {
